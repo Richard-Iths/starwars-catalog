1b688fb7cf6c38b413befe33e4bedbb41e2e6f04
richard-iths <richard.forsberg@iths.se>
2020-10-30 15:11:25 +0100

    Tab page added
    Co-authored-by: Elias Lundgren <elias.lundgren@iths.se>

--- public/js/app.js
+++ public/js/app.js
@@ -1,53 +1,117 @@
 async function main() {
-  let pagination = 1;
   const iteratorPrev = document.querySelector(".list-iterator__prev");
   const iteratorCurrent = document.querySelector(".list-iterator__current");
   const iteratorMax = document.querySelector(".list-iterator__of");
   const iteratorNext = document.querySelector(".list-iterator__next");
-  const charactersLoader = document.querySelector(".characters-loader");
-  const characterInfoLoader = document.querySelector(".character-info-loader");
-  const planetInfoLoader = document.querySelector(".planet-info-loader");
   const characterList = document.querySelector(".star-wars-characters");
   const characterInfo = document.querySelector(".star-wars-character-details");
   const planetInfo = document.querySelector(".star-wars-character-planet");
 
+  let pagination = 1;
+  const cache = {};
   let people = await getPeople(pagination);
-  async function getPeople(pagination) {
-    const response = await fetch(
-      `https://swapi.dev/api/people/?page=${pagination}`
-    );
-    return await response.json();
+  let person = people.results[0];
+  // let planet = await getHomeworld(person);
+  let planet = await getInfoData(person["homeworld"]);
+  const maxPage = Math.ceil(people.count / 10);
+  renderCharacterInfo(person);
+  renderPlanetInfo(planet[0]);
+  addInfoTabMenuEvents(person);
+  renderList(people.results);
+  iteratorMax.innerText = maxPage;
+  document.querySelector(".btn-star-wars-planet").focus();
+
+  async function isCached(url) {
+    if (cache[url]) {
+      return cache[url];
+    } else {
+      const response = await fetch(url);
+      const responseJson = await response.json();
+      cache[url] = responseJson;
+      return responseJson;
+    }
   }
 
-  async function getHomeworld(person) {
-    const response = await fetch(person.homeworld);
-    return await response.json();
+  async function getPeople(pagination) {
+    const url = `https://swapi.dev/api/people/?page=${pagination}`;
+    return await isCached(url);
+  }
+
+  // async function getHomeworld(person) {
+  //   const url = person.homeworld;
+  //   return isCached(url);
+  // }
+
+  function addInfoTabMenuEvents(person) {
+    const btnMenu = document.querySelectorAll(".star-wars-info-tab button");
+    btnMenu.forEach((btn) =>
+      btn.addEventListener("click", async (e) => {
+        const api = e.target.dataset.apiKey;
+        let infoData;
+        await renderLoader("character-info-details-loader", async () => {
+          infoData = await getInfoData(person[api]);
+        });
+        infoTabMenu(api, infoData);
+      })
+    );
+  }
+
+  async function getInfoData(url) {
+    const urls = url;
+    let data = [];
+    if (typeof urls === "object") {
+      for (let url of urls) {
+        data.push(await isCached(url));
+      }
+    } else {
+      data.push(await isCached(urls));
+    }
+    return data;
+  }
+  function infoTabMenuRender(data, callback) {
+    planetInfo.innerHTML = "";
+    if (data.length) {
+      data.forEach((item) => callback(item));
+    } else {
+      planetInfo.innerHTML = `<li class="list-title">No Data</li>`;
+    }
+  }
+  function infoTabMenu(id, data) {
+    switch (id) {
+      case "species":
+        infoTabMenuRender(data, renderSpeciesInfo);
+        break;
+      case "starships":
+        infoTabMenuRender(data, renderStarshipsInfo);
+        break;
+      case "vehicles":
+        infoTabMenuRender(data, renderVehiclesInfo);
+        break;
+      default:
+        infoTabMenuRender(data, renderPlanetInfo);
+    }
   }
 
   function renderList(people) {
-    let index = 0;
-    for (person of people) {
+    for (index in people) {
       const listItem = document.createElement("li");
-      listItem.innerText = person.name;
-      listItem.id = index;
+      listItem.innerText = people[index].name;
+      listItem.dataset.id = index;
       listItem.addEventListener("click", addCharacterInfo);
       characterList.appendChild(listItem);
-      index++;
     }
-    charactersLoader.classList.add("hidden");
   }
 
   async function addCharacterInfo(e) {
-    characterInfo.innerHTML = "";
-    planetInfo.innerHTML = "";
-    characterInfoLoader.classList.remove("hidden");
-    planetInfoLoader.classList.remove("hidden");
-    const person = people.results[e.target.id];
-    const planet = await getHomeworld(person);
-    renderCharacterInfo(person);
-    renderPlanetInfo(planet);
-    characterInfoLoader.classList.add("hidden");
-    planetInfoLoader.classList.add("hidden");
+    renderLoader("character-info-loader", async () => {
+      person = people.results[e.target.dataset.id];
+      // planet = await getHomeworld(person);
+      planet = await getInfoData(person.homeworld);
+      addInfoTabMenuEvents(person);
+      renderCharacterInfo(person);
+      renderPlanetInfo(planet[0]);
+      document.querySelector(".btn-star-wars-planet").focus();
+    });
   }
   function renderCharacterInfo(character) {
     characterInfo.innerHTML = `
@@ -75,36 +139,70 @@ async function main() {
     <li>Terrain: ${planet.terrain}</li>
     `;
   }
-  let person = people.results[0];
-  let planet = await getHomeworld(person);
-  renderCharacterInfo(person);
-  renderPlanetInfo(planet);
-  renderList(people.results);
-  iteratorMax.innerText = Math.ceil(people.count / 10);
+  function renderStarshipsInfo(ships) {
+    planetInfo.innerHTML += `
+    <li class="list-title mt-2">${ships.name}</li>
+    <li>Model: ${ships.model}</li>
+    <li>Manufacturer: ${ships.manufacturer}</li>
+    <li>Max Atmosphering Speed : ${ships.max_atmosphering_speed}</li>
+    `;
+  }
 
-  const iteratorEventCallback = async (conditionCallback, callback) => {
-    if (conditionCallback) {
-      characterList.innerHTML = "";
-      charactersLoader.classList.remove("hidden");
-      pagination = callback();
+  function renderSpeciesInfo(species) {
+    planetInfo.innerHTML += `
+    <li class="list-title">${species.name}</li>
+    <li>Classification: ${species.classification}</li>
+    <li>Designation: ${species.designation}</li>
+    <li>Average Lifespan : ${species.average_lifespan}</li>
+    `;
+  }
+
+  function renderVehiclesInfo(vehicles) {
+    planetInfo.innerHTML += `
+    <li class="list-title mt-2">${vehicles.name}</li>
+    <li>Model: ${vehicles.model}</li>
+    <li>Manufacturer: ${vehicles.manufacturer}</li>
+    <li>Max Atmosphering Speed : ${vehicles.max_atmosphering_speed}</li>
+    `;
+  }
+
+  function toggleLoaderClasses(loaderElement, toggleClass) {
+    loaderElement.forEach((loader) => loader.classList.toggle(toggleClass));
+  }
+  async function renderLoader(loaderClass, callback) {
+    const loaders = document.querySelectorAll(`.${loaderClass}`);
+    toggleLoaderClasses(loaders, "hidden");
+    await callback();
+    toggleLoaderClasses(loaders, "hidden");
+  }
+
+  const setPage = async () => {
+    characterList.innerHTML = "";
+    renderLoader("characters-loader", async () => {
       people = await getPeople(pagination);
       renderList(people.results);
       iteratorCurrent.innerText = pagination;
-    }
+    });
   };
+  // forward and backward button click event
+  iteratorPrev.addEventListener("click", () => {
+    if (pagination - 1 !== -0) {
+      pagination--;
+    } else {
+      pagination = maxPage;
+    }
 
-  iteratorPrev.addEventListener("click", async () => {
-    await iteratorEventCallback(
-      () => !(pagination == 1),
-      () => pagination - 1
-    );
+    setPage();
   });
 
-  iteratorNext.addEventListener("click", async () => {
-    await iteratorEventCallback(
-      () => !(pagination == Math.ceil(people.count / 10)),
-      () => pagination + 1
-    );
+  iteratorNext.addEventListener("click", () => {
+    if (pagination + 1 !== maxPage + 1) {
+      pagination++;
+    } else {
+      pagination = 1;
+    }
+
+    setPage();
   });
 }
 
